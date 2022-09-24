52f99f6e4f419e9d0238f7dc80f4c740d98e9c67
Abdullah Khasawneh <abdullahkhasawneh@hotmail.com>
2022-09-24 15:38:04 +0900

    added dependency installation shell script for RHEL-based OS's

---
+++ seurat-dependencies-rhel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo yum update
+sudo yum upgrade
+sudo yum install curl
+sudo yum install openssl
+sudo yum install libcurl4-openssl-dev
+sudo yum install r-base
+sudo yum install libhdf5-dev
+sudo yum install libhdf5-serial-dev
+sudo yum install h5utils
+sudo yum install hdf5-tools
+sudo yum install hdf5-helpers
